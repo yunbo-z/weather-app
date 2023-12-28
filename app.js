204d4c27112851e8b187d3ef12b70bdc566368bd
yunbo-z <zyunbo0330@gmail.com>
2023-12-28 23:16:15 +0100

    add geocode callback function

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 const request = require('postman-request')
+const geocode = require('./utils/geocode.js')
 
-const url = 'http://api.weatherstack.com/current?access_key=48781b670b72b9bbaa54a51f7be4f331&query=48.8566,2.3522&units=m'
+// const url = 'http://api.weatherstack.com/current?access_key=48781b670b72b9bbaa54a51f7be4f331&query=48.8566,2.3522&units=m'
 
-request({ url:url, json: true }, (error,response) => {
-    if (error) {
-        console.log('Unable to connect to weather server!')
-    }else if (response.body.error) {
-        console.log('Unable to find location!')
-    }
-    else{
-        console.log('In ' + response.body.location.name + ', the weather is ' + response.body.current.weather_descriptions +', it\'s currently ' + response.body.current.temperature + ' degree.')
-    }
-})
+// request({ url:url, json: true }, (error,response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather server!')
+//     }else if (response.body.error) {
+//         console.log('Unable to find location!')
+//     }
+//     else{
+//         console.log('In ' + response.body.location.name + ', the weather is ' + response.body.current.weather_descriptions +', it\'s currently ' + response.body.current.temperature + ' degree.')
+//     }
+// })
 
-// geocoode
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoieWIwMCIsImEiOiJjbHFtc3Y5aG0xcGl1MmtyeXhjem9wZ3ZmIn0.46R4e0SOkD-qFjQVU_NVeQ&limit=1'
+// // geocoode
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoieWIwMCIsImEiOiJjbHFtc3Y5aG0xcGl1MmtyeXhjem9wZ3ZmIn0.46R4e0SOkD-qFjQVU_NVeQ&limit=1'
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to GeoCode server!')
-    } else if (response.body.features.length ===0) {
-        console.log('Unable to find location!')
-    }
-    else {
-        const laititude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(laititude, longitude)
-    }
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to GeoCode server!')
+//     } else if (response.body.features.length ===0) {
+//         console.log('Unable to find location!')
+//     }
+//     else {
+//         const laititude = response.body.features[0].center[1]
+//         const longitude = response.body.features[0].center[0]
+//         console.log(laititude, longitude)
+//     }
+// })
+
+
+geocode('boston', (error, data) => {
+    console.log('error', error)
+    console.log('data', data)
 })
