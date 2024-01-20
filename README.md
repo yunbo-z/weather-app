eaa5dfdd95f1c1fc3411f9e78b870be29c4d8c66
yunbo-z <zyunbo0330@gmail.com>
2024-01-20 17:54:24 +0100

    hbs

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ using WeatherStack which is a free weather API;
 Chrome extension: JSON fomatter;
 
 mapbox geocoding service;
+
+web-server:
+    handlebars: npm package, like a plugin in Express js.
+    hbs: npm package, deal with dynamic content
+    HTTP request methods:
+        GET: requests a representation of the specified resource.
+        HEAD:  
+        POST:
+        PUT:
+        DELETE:
+        CONNECT:
+        OPTIONS:
+        TRACE:
+        PATCH:
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const path = require('path')//path is the core module in nodejs so we don't need to install it
 
-
-// __filename and __dirname are used to get the filename and directory name of the currently executing file
-console.log(__dirname)
-console.log(__filename)
-console.log(path.join(__dirname, '../public'))
-
 const app = express()
-//customize your server
-app.use(express.static(path.join(__dirname, '../public')))
+
+//define paths for Express config
+// __filename and __dirname are used to get the filename and directory name of the currently executing file
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates')
+
+//setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+
+//setup static directory to serve
+//app.use:customize your server
+//static: the assets are static
+app.use(express.static(publicDirectoryPath))
 
 //Routing refers to determining how an application responds to a client request to a particular endpoint
 //app.METHOD(PATH, HANDLER)
@@ -24,6 +30,12 @@ app.use(express.static(path.join(__dirname, '../public')))
 // app.get('/about', (req, res) => {
 //     res.send('<h1>about page</h1>')
 // })
+app.get('', (req, res) => {
+    res.render('index', {
+        name: 'lili'
+    })
+})
+
 app.get('/weather', (req, res) => {
     res.send({
         forecast:'for',
@@ -31,6 +43,11 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'Here is the help page'
+    })
+})
 
 app.listen(3000, () => {
     console.log('server is up on port 3000.')
