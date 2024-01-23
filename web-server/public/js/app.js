8e420a4dfa209a24fd3ccc04de8ec74203b31ea9
yunbo-z <zyunbo0330@gmail.com>
2024-01-23 18:51:04 +0100

    the message showing on the web interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -7,16 +7,24 @@
 // querySelector method returns the first element(like: <form>, <input>) that matches a CSS selector
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const locationMessage = document.querySelector('#location-message')
+const forecastMessage = document.querySelector('#forecast-message')
+
+
 
 // thr function run when the event occurs
 weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault()//prevent reload the whole page after event occurs
+    e.preventDefault()//prevent the event happened after event occurs
     const location = search.value
+    locationMessage.textContent = 'Loading...'
+    forecastMessage.textContent = ''
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error)
+                locationMessage.textContent = data.error
             } else {
+                locationMessage.textContent = 'location: '+ data.location
+                forecastMessage.textContent = 'forecast: ' + data.forecast
                 console.log('location: '+ data.location)
                 console.log('forecast: ' + data.forecast)
             }
