92262c97a063cd6334c80fc70349f95f059a2e94
yunbo-z <zyunbo0330@gmail.com>
2024-01-25 23:07:30 +0100

    annoying mongodb dont wotk

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,5 +1,17 @@
 //CRUD create read update delete
 
 const mongodb = require('mongodb')
+//initialize the connection
 const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect  to database')
+    }
+
+    console.log('Connected correctly!')
+})
 
