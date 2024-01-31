173a854aade8e924b7f2ea1d90c9ed46ca05ccce
yunbo-z <zyunbo0330@gmail.com>
2024-01-31 18:27:53 +0100

    connect to the mongodb server successfully and insert the first object for the user collection which belongs to the task-manager database

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -4,18 +4,48 @@ const mongodb = require('mongodb')
 //initialize the connection
 const MongoClient = mongodb.MongoClient
 
-//const connectionURL = 'mongodb://root:your_own_password@localhost:27017/weatherData.city/?authMechanism=DEFAULT'
-// const connectionURL='mongodb://root:your_own_password@localhost:27017/weatherData'
-const connectionURL = 'mongodb://root:your_own_password@127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1'
+// //const connectionURL = 'mongodb://root:your_own_password@localhost:27017/weatherData.city/?authMechanism=DEFAULT'
+// // const connectionURL='mongodb://root:your_own_password@localhost:27017/weatherData'
+const connectionURL = 'mongodb://root:your_own_password@127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.3'
 const databaseName = 'task-manager'
 
 
-MongoClient.connect(connectionURL, {}, (error, client) => {
-    if (error) {
-        console.log('Unable to connect  to database')
-    }
-    console.log('Connected correctly!')
-})
+// MongoClient.connect(connectionURL, (error, client) => {
+//     if (error) {
+//         return console.log('Unable to connect  to database')
+//     }
+//     const db = client.db(databaseName)
+
+//     db.collection('city').insertOne({
+//         location: 'Paris',
+//         country: 'France'
+//     })
+// })
+
+const client = new MongoClient(connectionURL)
+async function main() {
+    //use connect method to connect to the server
+    await client.connect();
+    console.log('connected successfully to the server!')
+
+    const db = client.db(databaseName)
+    //insertOne method expect a object as its first argument
+    const collection = db.collection('users')
+   
+    const insertResult = await collection.insertOne({
+        name: 'YB',
+        age: 23
+    })
+    console.log('Inserted Documents => ', insertResult)
+   
+    return 'done'
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
+
 
 // const { MongoClient } = require("mongodb");
 
