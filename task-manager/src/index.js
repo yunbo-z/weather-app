e6462af22ce44b643e95fc56c7bcf59e2902b0e9
yunbo-z <zyunbo0330@gmail.com>
2024-02-07 23:27:08 +0100

    read tasks endpoint

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -28,6 +28,49 @@ app.post('/tasks', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks', (req,res) => {
+    Tasks.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((err) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Tasks.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((err) => {
+        res.status(500).send()
+    })
+})
+
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
