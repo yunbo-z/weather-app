b074e5796369ed328f9b8e7d9a7f5de29f8426bc
yunbo-z <zyunbo0330@gmail.com>
2024-02-21 14:33:31 +0100

    delete task app from this file

--- task-manager/src/db/mongoose.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose")
-
-const url = 'mongodb://root:your_own_password@127.0.0.1:27017/task-manager-api?authSource=admin'
-mongoose.connect(url).catch((err) => console.log(err))
-
